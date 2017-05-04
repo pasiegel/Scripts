80a772ebd3e163673c9187094a001a697d469498
pasiegel <pasiegel@gmail.com>
2017-05-03 21:17:30 -0400

    Update user_setup2.sh

--- user_setup2.sh
+++ user_setup2.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 pulseaudio --start
-# old packer cmds
-#wget https://aur.archlinux.org/cgit/aur.git/snapshot/packer.tar.gz
-#tar -xvzf packer.tar.gz
-#cd packer
-#makepkg --noconfirm -si
-#cd ..
-#rm -rf packer/
-#rm packer.tar.gz
-
-#packer --noconfirm -S cli-visualizer neomutt i3-gaps ttf-font-awesome vim-pathogen vim-live-latex-preview projectm-pulseaudio taffy neofetch i3lock tamzen-font-git speedometer nodejs-markdown-pdf
 #COPY CONFIG FILES
 unzip rice.zip
 rsync -va --delete-after rice/ ~
