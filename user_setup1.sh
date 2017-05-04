17082b734913bfe4d83ecb1b83054ce97476ae41
pasiegel <pasiegel@gmail.com>
2017-05-03 21:13:40 -0400

    Update user_setup1.sh

--- user_setup1.sh
+++ user_setup1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#build packer for aur support
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/packer.tar.gz
 tar -xvzf packer.tar.gz
 cd packer
@@ -7,7 +7,7 @@ makepkg --noconfirm -si
 cd ..
 rm -rf packer/
 rm packer.tar.gz
-
+# install required aurs
 packer --noconfirm -S cli-visualizer neomutt i3-gaps ttf-font-awesome vim-pathogen vim-live-latex-preview projectm-pulseaudio taffy neofetch i3lock tamzen-font-git speedometer nodejs-markdown-pdf lightdm-slick-greeter nomachine-enterprise-client
-
+# run rsync for config files
 bash user_setup2.sh
